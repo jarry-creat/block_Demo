b39d8ddcad5ca50d3b6eab0a50691673dd285a56
Luban <1401172246@qq.com>
2023-08-27 00:48:10 +0800

    blockchain

---
+++ README.md
@@ -0,0 +1,21 @@
+### 安装依赖(Install dependencies)
+
+```
+npm install --save
+yarn
+```
+
+如果作为依赖安装, it's published to npm, so if you want to install it as an dependency, please use
+
+```
+npm install --save luotuocoin
+or
+yarn add luotuocoin
+```
+
+### 运行测试用例(Run tests)
+
+```cmd
+npm run test
+or
+yarn test
---
+++ bilibiliDemo/main.js
@@ -0,0 +1,28 @@
+const ecLib = require('elliptic').ec;
+const ec = new ecLib('secp256k1') // curve name
+// 如果作为依赖安装，请用这种方式import
+// const { Transaction, Chain, Block } = require("luotuocoin");
+const { Transaction, Chain, Block } = require("./blockChainDemo");
+
+const luotuoCoin = new Chain(3);
+const keyPairSender = ec.genKeyPair();
+const privateKeySender = keyPairSender.getPrivate('hex')
+const publicKeySender = keyPairSender.getPublic('hex')
+
+const keyPairReceiver = ec.genKeyPair();
+const privateKeyReceiver = keyPairReceiver.getPrivate('hex')
+const publicKeyReceiver = keyPairReceiver.getPublic('hex')
+
+const t1 = new Transaction(publicKeySender, publicKeyReceiver, 10);
+t1.sign(ec.keyFromPrivate(privateKeySender))
+console.log(t1)
+// t1.amount=20
+// const t2 = new Transaction("addr2", "addr1", 5);
+luotuoCoin.addTransaction(t1);
+// luotuoCoin.addTransaction(t2);
+
+// console.log(luotuoCoin)
+luotuoCoin.mineTransactionPool("addr3");
+console.log(luotuoCoin.validateChain())
+console.log(luotuoCoin)
+console.log(luotuoCoin.chain[1])
